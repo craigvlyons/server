f0b8a78c6d7d5cafc78f9e73d8f3eb8e18f974a7
Craig Lyons <craigvlyons@gmail.com>
2022-10-08 10:18:57 -0600

    Finalized tutorial project

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,48 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
+use std::fs;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler{
+    public_path: String
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self{
+        Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String>{
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path){
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {                
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,            
+        }
+        
+    }
+}
 
 impl Handler for WebsiteHandler{
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::OK, Some("<h1>Test</h1>".to_string()))
+        // Response::new(StatusCode::OK, Some("<h1>Test</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::OK, self.read_file("idex.html")),
+                "/hello" => Response::new(StatusCode::OK, self.read_file("hello.html")),
+                path => match self.read_file(path){
+                    Some(contents) => Response::new(StatusCode::OK, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
+                
+            }
+            _=> Response::new(StatusCode::NotFound, None),
+        }
     }
 }
